1fb781bb4dc3e763a283ded08662f32bd58dbdab
Muhammad-Shahr0zMuhammad-Shahr0z <shahroxyt@gmail.com>
2024-03-08 19:48:37 +0500

    first commit

---
+++ 29.assigment/main.js
@@ -0,0 +1,20 @@
+"use strict";
+// Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+// • Make a array of your three favorite fruits and call it favorite_fruits.
+// • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+let favoriteFruites = ["APPLE", "MANGO", "BANANA"];
+if (favoriteFruites.includes("APPLE")) {
+    console.log(`I really like APPLE`);
+}
+if (favoriteFruites.includes("MANGO")) {
+    console.log(`I really like MANGO`);
+}
+if (favoriteFruites.includes("ORANGE")) {
+    console.log(`I really like ORANGE `);
+}
+if (favoriteFruites.includes("BANANA")) {
+    console.log(`I really like BANANA`);
+}
+if (favoriteFruites.includes("BLURBERRY")) {
+    console.log(`I really like BLUEBERRY`);
+}
