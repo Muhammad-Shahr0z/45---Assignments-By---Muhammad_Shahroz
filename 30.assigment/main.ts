1f4524f08d7178f9a34596435476f60be858c119
Muhammad-Shahr0zMuhammad-Shahr0z <shahroxyt@gmail.com>
2024-03-08 20:08:52 +0500

    first commit

--- 30.assigment/main.ts
+++ 30.assigment/main.ts
@@ -8,7 +8,7 @@
 // // • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
 
 
-// // • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+// // • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again
 
 
 
