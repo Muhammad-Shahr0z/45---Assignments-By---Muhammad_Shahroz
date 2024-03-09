048a4b6d3e13f15dd61b8b10daf889fcd31617f8
Muhammad-Shahr0zMuhammad-Shahr0z <shahroxyt@gmail.com>
2024-03-10 00:16:22 +0500

    first commit

---
+++ 31.assigment/main.js
@@ -0,0 +1,10 @@
+"use strict";
+// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
+let userName = ["SHAHROZ", "USAMA", "NAJAM", "ADMIN", "ALI", "AQIB"];
+if (userName.length === 6) {
+    userName = [];
+    console.log(`Remove all of the usernames from the array `);
+    console.log("We need to find some users! " + userName.length);
+}
