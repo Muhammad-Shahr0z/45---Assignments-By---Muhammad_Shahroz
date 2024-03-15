df2e7b25c5e1683380e2529ad7c245839a3d2e0e
Muhammad-Shahr0zMuhammad-Shahr0z <shahroxyt@gmail.com>
2024-03-15 20:17:38 +0500

    first commit

---
+++ 41.assigment/main.ts
@@ -0,0 +1,24 @@
+// Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+
+
+
+let Magicians:string[] = ["Harry Houdini" , "David Blaine", "Doug Henning" ,"Dai Vernon"]
+
+
+
+
+function show_magicians(magicianName:string[]){
+    for(let i = 0 ; i < magicianName.length ; i++)              //for loop  
+    
+    console.log(magicianName[i]);
+
+    //     magicianName.forEach(element => {                   //for each loop
+        
+        // console.log(element);
+        //  });
+        
+}
+
+show_magicians(Magicians);
+
+
